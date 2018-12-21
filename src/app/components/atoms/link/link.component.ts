e8e2657c6fff2fd68ca32920a86f0acbaa0e9867
mehmetalp <mehmetakifalp@gmail.com>
2018-12-21 18:44:38 +0200

    link component

---
+++ src/app/components/atoms/link/link.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy, ViewEncapsulation } from "@angular/core";
+
+@Component({
+    selector: 'baby-link',
+    templateUrl: 'link.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None
+})
+export class LinkComponent {
+    
+}
