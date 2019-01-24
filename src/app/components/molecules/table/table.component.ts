7dca597945957126a737b2bd8c53b62301dcae43
mehmetalp <mehmetakifalp@gmail.com>
2019-01-24 21:57:58 +0200

    table component

--- src/app/components/molecules/table/table.component.ts
+++ src/app/components/molecules/table/table.component.ts
@@ -2,12 +2,18 @@
  * @Author: mehmetakifalp@gmail.com - mehmet akif alp
  * @Company : rastmobile.com
  */
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'baby-table',
   templateUrl: 'table.component.html'
 })
 export class TableComponent{
+
+
+  @Input()
+  data = [{
+    id: 1, name: "Mehmet"
+  }];
 
 }
