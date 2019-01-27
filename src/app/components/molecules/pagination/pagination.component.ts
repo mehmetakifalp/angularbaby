1d5fb510d6973e7d7ea6c8c58b6b37703ef123e0
mehmetalp <mehmetakifalp@gmail.com>
2019-01-27 19:10:05 +0200

    pagination

---
+++ src/app/components/molecules/pagination/pagination.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'baby-pagination',
+    templateUrl: 'pagination.component.html'
+})
+export class PaginationComponent{
+
+}
