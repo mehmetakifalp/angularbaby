37bca8ab867b00d06e59861c46b2391f07875bc7
mehmetalp <mehmetakifalp@gmail.com>
2019-01-27 19:10:51 +0200

    pagination inputs

--- src/app/components/molecules/pagination/pagination.component.ts
+++ src/app/components/molecules/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'baby-pagination',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
 })
 export class PaginationComponent{
 
+
+    @Input()
+    pageSize: number = 0;
+
+    @Input()
+    totalCount: number = 0;
 }
