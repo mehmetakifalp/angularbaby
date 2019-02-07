ee11f739eed9cae06fb6d70743b6ef74f8a692ef
mehmetakifalp <mehmetakifalp@gmail.com>
2019-02-07 17:41:35 +0300

    extends input component

--- src/app/components/atoms/checkbox/checkbox.component.ts
+++ src/app/components/atoms/checkbox/checkbox.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../input/input.component';
 
 @Component({
   selector:'baby-checkbox',
   templateUrl:'checkbox.component.html'
 })
-export class CheckboxComponent{
+export class CheckboxComponent extends InputComponent{
 
 }
