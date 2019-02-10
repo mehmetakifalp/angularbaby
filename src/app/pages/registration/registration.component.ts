b5672db749285e9c98be6d14c353ac7323532043
mehmetalp <mehmetakifalp@gmail.com>
2019-02-10 21:42:23 +0200

    first pages

---
+++ src/app/pages/registration/registration.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'registration',
+    templateUrl:'registration.component.html'
+})
+export class RegistrationComponent{
+    
+}
